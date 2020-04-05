2668659f633899e4680565821556b3df9f87f8c7
Des Dae <desdae@gmail.com>
2020-04-05 14:11:33 +0300

    1342. Number of Steps to Reduce a Number to Zero

---
+++ CSharpSolutions/CSharpSolutions/01342/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._01342
+{
+    public class Solution
+    {
+        public int NumberOfSteps(int num)
+        {
+            int steps = 0;
+            while (num > 0)
+            {
+                if (num % 2 == 0) num /= 2;
+                else num--;
+                steps++;
+            }
+            return steps;
+        }
+    }
+}
