0a5628569fcb2ffb479b149f7b2001ee05649131
Des Dae <desdae@gmail.com>
2020-04-05 15:24:09 +0300

    1362. Closest Divisors

---
+++ CSharpSolutions/CSharpSolutions/01300/01362/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpSolutions._01300._01362
+{
+    public class Solution
+    {
+        private int[] CD(int num)
+        {
+            int s = (int)Math.Sqrt(num);
+            int r = s;
+            while (s > 0)
+            {
+                if (num % s == 0)
+                {
+                    r = num / s;
+                    break;
+                }
+                s--;
+            }
+            return new int[] { s, r };
+        }
+        public int[] ClosestDivisors(int num)
+        {
+            var a = CD(num + 1);
+            var b = CD(num + 2);
+            if (Math.Abs(a[0] - a[1]) < Math.Abs(b[0] - b[1]))
+                return a;
+            else
+                return b;
+        }
+    }
+}
