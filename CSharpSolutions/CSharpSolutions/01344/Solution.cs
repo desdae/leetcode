9f52742767a52bdded244f1e70307392554c1331
Des Dae <desdae@gmail.com>
2020-04-05 14:13:27 +0300

    1344. Angle Between Hands of a Clock

---
+++ CSharpSolutions/CSharpSolutions/01344/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._01344
+{
+    public class Solution
+    {
+        public double AngleClock(int hour, int minutes)
+        {
+            double minAngle = ((double)minutes * 6.0) % 360.0;
+            double hourAngle = ((double)hour * 30.0 + (double)minutes * 0.5) % 360.0;
+
+            double res = 500.0;
+            res = Math.Min(res, Math.Abs(minAngle - hourAngle));
+            res = Math.Min(res, Math.Abs(minAngle + 360.0 - hourAngle));
+            res = Math.Min(res, Math.Abs(hourAngle + 360.0 - minAngle));
+
+            return res;
+        }
+    }
+}
