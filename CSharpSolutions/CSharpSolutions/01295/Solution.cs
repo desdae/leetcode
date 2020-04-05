72e6b0fbddc9c6d99a9b02878a2b7b35dc5bc65e
Des Dae <desdae@gmail.com>
2020-04-05 13:57:09 +0300

    1295. Find Numbers with Even Number of Digits

---
+++ CSharpSolutions/CSharpSolutions/01295/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._01295
+{
+    public class Solution
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int res = 0;
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i].ToString().Length % 2 == 0)
+                    res++;
+            return res;
+        }
+    }
+}
