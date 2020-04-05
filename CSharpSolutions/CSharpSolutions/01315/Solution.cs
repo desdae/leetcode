01d37fe477f1fa009ab794e04633e2993b571bd0
Des Dae <desdae@gmail.com>
2020-04-05 13:54:55 +0300

    1315. Sum of Nodes with Even-Valued Grandparent

---
+++ CSharpSolutions/CSharpSolutions/01315/Solution.cs
@@ -0,0 +1,25 @@
+﻿using CSharpSolutions.Common;
+
+namespace CSharpSolutions._01315
+{
+    public class Solution
+    {
+        int sum;
+        public void Traverse(TreeNode root, TreeNode parent, TreeNode grandParent)
+        {
+            if (root == null) return;
+
+            Traverse(root.left, root, parent);
+            Traverse(root.right, root, parent);
+
+            if (grandParent != null && grandParent.val % 2 == 0)
+                sum += root.val;
+        }
+        public int SumEvenGrandparent(TreeNode root)
+        {
+            sum = 0;
+            Traverse(root, null, null);
+            return sum;
+        }
+    }
+}
