28fc1bad877ee2da4fd98b646461c4fdbb5495bc
Des Dae <desdae@gmail.com>
2020-04-05 15:17:22 +0300

    1359. Count All Valid Pickup and Delivery Options

---
+++ CSharpSolutions/CSharpSolutions/01300/01359/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpSolutions._01300._01359
+{
+    public class Solution
+    {
+        public int CountOrders(int n)
+        {
+            int m = 1;
+            long res = 1;
+            for (int i = 1; i < n * 2 - 1; i += 2)
+            {
+                m += 2 * i + 3;
+                res = (res * m) % 1000000007;
+            }
+            return (int)res;
+        }
+    }
+}
