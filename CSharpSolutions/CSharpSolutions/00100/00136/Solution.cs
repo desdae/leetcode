c1bca7e9ee94461686280db309a2f782c2fd18cf
Des Dae <desdae@gmail.com>
2020-04-05 14:44:56 +0300

    136. Single Number

---
+++ CSharpSolutions/CSharpSolutions/00100/00136/Solution.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace CSharpSolutions._00100._00136
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums) => nums.Aggregate(0, (a, x) => a ^ x);
+    }
+}
