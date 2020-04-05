761c31e113fbe0158474a0d3dce6f502b560bac6
Des Dae <desdae@gmail.com>
2020-04-05 15:05:49 +0300

    219. Contains Duplicate II

---
+++ CSharpSolutions/CSharpSolutions/00200/00219/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._00200._00219
+{
+    public class Solution
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, List<int>> minMax = new Dictionary<int, List<int>>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (!minMax.ContainsKey(nums[i]))
+                {
+                    minMax[nums[i]] = new List<int>();
+                }
+                minMax[nums[i]].Add(i);
+            }
+            foreach (var x in minMax.Values)
+            {
+                if (x.Count >= 2)
+                {
+                    x.Sort();
+                    var a = x.ToArray();
+                    for (int i = 0; i < a.Length - 1; i++)
+                    {
+                        if (a[i + 1] - a[i] <= k)
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
