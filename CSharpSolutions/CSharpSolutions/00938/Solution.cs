26fc22ed673525707312f422262151b810577efd
Des Dae <desdae@gmail.com>
2020-04-05 14:00:02 +0300

    938. Range Sum of BST

---
+++ CSharpSolutions/CSharpSolutions/00938/Solution.cs
@@ -0,0 +1,13 @@
+﻿using CSharpSolutions.Common;
+
+namespace CSharpSolutions._00938
+{
+    public class Solution
+    {
+        public int RangeSumBST(TreeNode root, int L, int R)
+        {
+            if (root == null) return 0;
+            return RangeSumBST(root.left, L, R) + RangeSumBST(root.right, L, R) + ((root.val >= L && root.val <= R) ? root.val : 0);
+        }
+    }
+}
