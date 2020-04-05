0ea694c2436b7ff147a7ecbcf9b7baa7847946bb
Des Dae <desdae@gmail.com>
2020-04-05 13:40:10 +0300

    104. Maximum Depth of Binary Tree

---
+++ CSharpSolutions/CSharpSolutions/00104/Solution.cs
@@ -0,0 +1,27 @@
+﻿using CSharpSolutions.Common;
+
+namespace CSharpSolutions._00104
+{
+    public class Solution
+    {
+        private int maxd;
+        private void Traverser(TreeNode root, int currD)
+        {
+            if (root == null) return;
+            currD++;
+            if (currD > maxd)
+            {
+                maxd = currD;
+            }
+            Traverser(root.left, currD);
+            Traverser(root.right, currD);
+        }
+
+        public int MaxDepth(TreeNode root)
+        {
+            maxd = 0;
+            Traverser(root, 0);
+            return maxd;
+        }
+    }
+}
