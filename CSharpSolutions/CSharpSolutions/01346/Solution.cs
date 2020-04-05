9d64509831d127a405e5ffe7d31a0aacc99bfc33
Des Dae <desdae@gmail.com>
2020-04-05 14:19:49 +0300

    1346. Check If N and Its Double Exist

---
+++ CSharpSolutions/CSharpSolutions/01346/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._01346
+{
+    public class Solution
+    {
+        public bool CheckIfExist(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+                for (int j = i + 1; j < n; j++)
+                    if (arr[i] == arr[j] * 2 || arr[i] * 2 == arr[j])
+                        return true;
+            return false;
+        }
+    }
+}
