6a38a9237ff04f5286ec0d339cb29e0b084046a9
Des Dae <desdae@gmail.com>
2020-04-05 13:47:15 +0300

    1313. Decompress Run-Length Encoded List

---
+++ CSharpSolutions/CSharpSolutions/01313/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpSolutions._01313
+{
+    public class Solution
+    {
+        public int[] DecompressRLElist(int[] nums)
+        {
+            var r = new List<int>();
+            var n = nums.Length / 2;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < nums[i * 2]; j++)
+                    r.Add(nums[i * 2 + 1]);
+            }
+            return r.ToArray();
+        }
+    }
+}
