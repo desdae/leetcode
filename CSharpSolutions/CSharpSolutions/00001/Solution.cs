6ac842783130e6187cb9a9cc26f07b736c599d3e
Des Dae <desdae@gmail.com>
2020-04-05 13:34:09 +0300

    1. Two Sum

---
+++ CSharpSolutions/CSharpSolutions/00001/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharpSolutions._00001
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] ret = new int[] { 0, 0 };
+            for (int i = 0; i < nums.Length - 1; i++)
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        ret[0] = i;
+                        ret[1] = j;
+                        return ret;
+                    }
+                }
+            return ret;
+        }
+    }
+}
