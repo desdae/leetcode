aeed605a78a49b496b6ab6834bbe1795ee44d4b0
Des Dae <desdae@gmail.com>
2020-04-05 15:19:10 +0300

    1360. Number of Days Between Two Dates

---
+++ CSharpSolutions/CSharpSolutions/01300/01360/Solution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpSolutions._01300._01360
+{
+    public class Solution
+    {
+        public int DaysBetweenDates(string date1, string date2)
+        {
+            DateTime startDate = Convert.ToDateTime(date1);
+            DateTime endDate = Convert.ToDateTime(date2);
+            return Math.Abs((endDate - startDate).Days);
+        }
+    }
+}
