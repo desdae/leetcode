cd207e37019370560e1b1137eaa6b692d421bdf8
Des Dae <desdae@gmail.com>
2020-04-05 14:25:25 +0300

    226. Invert Binary Tree

---
+++ CSharpSolutions/CSharpSolutions/00226/Solution.cs
@@ -0,0 +1,22 @@
+﻿using CSharpSolutions.Common;
+
+namespace CSharpSolutions._00226
+{
+    public class Solution
+    {
+        private void DFS(TreeNode root)
+        {
+            if (root == null) return;
+            var tmp = root.left;
+            root.left = root.right;
+            root.right = tmp;
+            DFS(root.left);
+            DFS(root.right);
+        }
+        public TreeNode InvertTree(TreeNode root)
+        {
+            DFS(root);
+            return root;
+        }
+    }
+}
