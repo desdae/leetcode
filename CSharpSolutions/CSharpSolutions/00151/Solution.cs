1ba15a7de32779861a7f03408f5e758748942b91
Des Dae <desdae@gmail.com>
2020-04-05 13:50:07 +0300

    151. Reverse Words in a String

---
+++ CSharpSolutions/CSharpSolutions/00151/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpSolutions._00151
+{
+    public class Solution
+    {
+        public string ReverseWords(string s)
+        {
+            string[] x = s.Trim().Split(' ');
+            string res = string.Empty;
+            int i = x.Length;
+            while (i-- > 0)
+                if (x[i] != String.Empty)
+                    res += x[i].Trim() + " ";
+            return res.Trim();
+        }
+    }
+}
