b551c8b8e7c7dbf9bcdd41f78750089acaa39100
Des Dae <desdae@gmail.com>
2020-04-05 15:30:49 +0300

    1365. How Many Numbers Are Smaller Than the Current Number

---
+++ CSharpSolutions/CSharpSolutions/01300/01365/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._01300._01365
+{
+    public class Solution
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            var res = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int c = 0;
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] < nums[i])
+                        c++;
+                }
+                res.Add(c);
+            }
+            return res.ToArray();
+        }
+    }
+}
