0329077f62f93d079154f338161f9a630fdf5d52
Des Dae <desdae@gmail.com>
2020-04-05 13:44:17 +0300

    771. Jewels and Stones

---
+++ CSharpSolutions/CSharpSolutions/00771/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpSolutions._00771
+{
+    public class Solution
+    {
+        public int NumJewelsInStones(string J, string S)
+        {
+            var jews = new HashSet<char>();
+            int res = 0;
+            for (int i = 0; i < J.Length; i++)
+                jews.Add(J[i]);
+            for (int i = 0; i < S.Length; i++)
+                if (jews.Contains(S[i])) res++;
+            return res;
+        }
+    }
+}
