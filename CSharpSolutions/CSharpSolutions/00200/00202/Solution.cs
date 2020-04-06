91728c2ae74144b5f5c2a2c4e2dbae4f13baa8b5
Des Dae <desdae@gmail.com>
2020-04-06 12:01:38 +0300

    202. Happy Number

---
+++ CSharpSolutions/CSharpSolutions/00200/00202/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpSolutions._00200._00202
+{
+    public class Solution
+    {
+        private int SumSqDig(int n)
+        {
+            int r = 0;
+            while (n >= 1)
+            {
+                int d = n % 10;
+                n /= 10;
+                r += d * d;
+            }
+            return r;
+        }
+
+        public bool IsHappy(int n)
+        {
+            var d = new HashSet<int>();
+            while (true)
+            {
+                n = SumSqDig(n);
+                if (n == 1) return true;
+                if (d.Contains(n)) return false;
+                d.Add(n);
+            }
+        }
+    }
+}
