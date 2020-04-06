8b7c0ca0bd5b322743d663d7091d322db6e996fc
Des Dae <desdae@gmail.com>
2020-04-06 11:40:11 +0300

    1385. Find the Distance Value Between Two Arrays

---
+++ CSharpSolutions/CSharpSolutions/01300/01385/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpSolutions._01300._01385
+{
+    public class Solution
+    {
+        public int FindTheDistanceValue(int[] arr1, int[] arr2, int d)
+        {
+            int r = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                bool b = true;
+                for (int j = 0; j < arr2.Length; j++)
+                {
+                    if (d >= Math.Abs(arr1[i] - arr2[j]))
+                    {
+                        b = false;
+                        break;
+                    }
+                }
+                if (b) r++;
+            }
+            return r;
+        }
+    }
+}
