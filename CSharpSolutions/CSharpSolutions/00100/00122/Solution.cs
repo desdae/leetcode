cfb5b51791134bb8a5cc0022ad17b263fcfa8c7f
Des Dae <desdae@gmail.com>
2020-04-06 12:24:25 +0300

    122. Best Time to Buy and Sell Stock II

---
+++ CSharpSolutions/CSharpSolutions/00100/00122/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharpSolutions._00100._00122
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int r = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                    r += prices[i] - prices[i - 1];
+            }
+            return r;
+        }
+    }
+}
