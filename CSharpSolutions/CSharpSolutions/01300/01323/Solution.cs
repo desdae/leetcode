fecfe902f3afda8c07c365a3ac75b3552d9f81e2
Des Dae <desdae@gmail.com>
2020-04-06 12:33:14 +0300

    1323. Maximum 69 Number

---
+++ CSharpSolutions/CSharpSolutions/01300/01323/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._01300._01323
+{
+    public class Solution
+    {
+        public int Maximum69Number(int num)
+        {
+            var sb = new StringBuilder(num.ToString());
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (sb[i] == '6')
+                {
+                    sb[i] = '9';
+                    break;
+                }
+            }
+            return int.Parse(sb.ToString());
+        }
+    }
+}
