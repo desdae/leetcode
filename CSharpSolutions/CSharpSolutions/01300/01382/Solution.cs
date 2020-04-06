e0ade6fb8662d2421d3165de33e9c725e995108c
Des Dae <desdae@gmail.com>
2020-04-06 11:38:16 +0300

    1382. Balance a Binary Search Tree

---
+++ CSharpSolutions/CSharpSolutions/01300/01382/Solution.cs
@@ -0,0 +1,39 @@
+﻿using CSharpSolutions.Common;
+using System.Collections.Generic;
+
+namespace CSharpSolutions._01300._01382
+{
+    public class Solution
+    {
+        public void ToList(TreeNode root, List<TreeNode> d)
+        {
+            if (root == null)
+                return;
+
+            ToList(root.left, d);
+            d.Add(root);
+            ToList(root.right, d);
+        }
+
+        public virtual TreeNode Build(List<TreeNode> d, int s, int e)
+        {
+            if (s > e)
+                return null;
+
+            int mid = (s + e) / 2;
+            TreeNode n = d[mid];
+
+            n.left = Build(d, s, mid - 1);
+            n.right = Build(d, mid + 1, e);
+
+            return n;
+        }
+        public TreeNode BalanceBST(TreeNode root)
+        {
+            var d = new List<TreeNode>();
+            ToList(root, d);
+
+            return Build(d, 0, d.Count - 1);
+        }
+    }
+}
