eb8a8906706e4564b66f7b214b1595754b990c6d
Des Dae <desdae@gmail.com>
2020-04-06 11:55:10 +0300

    1394. Find Lucky Integer in an Array

---
+++ CSharpSolutions/CSharpSolutions/01300/01394/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpSolutions._01300._01394
+{
+    public class Solution
+    {
+        public int FindLucky(int[] arr)
+        {
+            int m = -1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int c = 0;
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] == arr[i])
+                        c++;
+                }
+                if (c == arr[i])
+                {
+                    m = Math.Max(m, c);
+                }
+            }
+            return m;
+        }
+    }
+}
