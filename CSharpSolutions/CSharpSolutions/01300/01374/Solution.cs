7cc8c48fcc3c721a5516aaf3d3d9c72cc46791cb
Des Dae <desdae@gmail.com>
2020-04-06 11:15:05 +0300

    1374. Generate a String With Characters That Have Odd Counts

---
+++ CSharpSolutions/CSharpSolutions/01300/01374/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+
+public class Solution
+{
+    public string GenerateTheString(int n)
+    {
+        var res = new StringBuilder();
+        if (n % 2 == 1)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                res.Append('g');
+            }
+        }
+        else
+        {
+            for (int i = 0; i < n - 1; i++)
+            {
+                res.Append('g');
+            }
+            res.Append('x');
+        }
+        return res.ToString();
+    }
+}
