a4474fa56a2b8887367af2435311d44d9a28081c
Des Dae <desdae@gmail.com>
2020-04-06 11:48:11 +0300

    1389. Create Target Array in the Given Order

---
+++ CSharpSolutions/CSharpSolutions/01300/01389/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpSolutions._01300._01389
+{
+    public class Solution
+    {
+        public int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            int n = nums.Length;
+            int[] res = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n - 1; j > index[i]; j--)
+                {
+                    res[j] = res[j - 1];
+                }
+                res[index[i]] = nums[i];
+            }
+            return res;
+        }
+    }
+}
