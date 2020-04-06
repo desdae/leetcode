c7a73036f088aa0681c72602c0311c2e2207c6a4
Des Dae <desdae@gmail.com>
2020-04-06 12:15:27 +0300

    1403. Minimum Subsequence in Non-Increasing Order

---
+++ CSharpSolutions/CSharpSolutions/01400/01403/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpSolutions._01400._01403
+{
+    public class Solution
+    {
+        public IList<int> MinSubsequence(int[] nums)
+        {
+            Array.Sort(nums);
+            Array.Reverse(nums);
+            int sum = nums.Sum();
+            var r = new List<int>();
+            var cs = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                cs += nums[i];
+                r.Add(nums[i]);
+                if (cs > sum - cs)
+                    return r;
+            }
+            return r;
+        }
+    }
+}
