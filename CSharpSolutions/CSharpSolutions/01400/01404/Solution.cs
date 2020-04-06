01f4dabe9b339a851e8fd376aee6e51ae073505e
Des Dae <desdae@gmail.com>
2020-04-06 12:18:12 +0300

    1404. Number of Steps to Reduce a Number in Binary Representation to One

---
+++ CSharpSolutions/CSharpSolutions/01400/01404/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace CSharpSolutions._01400._01404
+{
+    public class Solution
+    {
+        public int NumSteps(string s)
+        {
+            BigInteger r = 0;
+
+            foreach (char c in s)
+            {
+                r <<= 1;
+                r += c == '1' ? 1 : 0;
+            }
+            int x = 0;
+            while (r > 1)
+            {
+                if (r % 2 == 0)
+                    r /= 2;
+                else
+                    r ++;
+                x++;
+            }
+            return x;
+        }
+    }
+}
