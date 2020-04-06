71330a64a32dbb4defbff282189796db003a4812
Des Dae <desdae@gmail.com>
2020-04-06 11:34:12 +0300

    1380. Lucky Numbers in a Matrix

---
+++ CSharpSolutions/CSharpSolutions/01300/01380/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpSolutions._01300._01380
+{
+    public class Solution
+    {
+        public IList<int> LuckyNumbers(int[][] matrix)
+        {
+            HashSet<int> md = new HashSet<int>();
+            for (int j = 0; j < matrix.Length; j++)
+            {
+                int m = int.MaxValue;
+                for (int i = 0; i < matrix[0].Length; i++)
+                {
+                    m = Math.Min(m, matrix[j][i]);
+                }
+                md.Add(m);
+            }
+            var res = new List<int>();
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                int m = int.MinValue;
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    m = Math.Max(m, matrix[j][i]);
+                }
+                if (md.Contains(m))
+                    res.Add(m);
+            }
+            return res;
+        }
+    }
+}
