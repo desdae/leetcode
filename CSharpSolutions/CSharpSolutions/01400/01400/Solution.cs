b76f83ea5b3fa52d6dc99c50b4125b88972e8113
Des Dae <desdae@gmail.com>
2020-04-06 12:07:57 +0300

    1400. Construct K Palindrome Strings

---
+++ CSharpSolutions/CSharpSolutions/01400/01400/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpSolutions._01400._01400
+{
+    public class Solution
+    {
+        public bool CanConstruct(string s, int k)
+        {
+            if (s.Length < k)
+                return false;
+            var d = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!d.ContainsKey(s[i]))
+                    d[s[i]] = 0;
+                d[s[i]]++;
+            }
+            int uneven = d.Values.Count(v => v % 2 == 1);
+            if (uneven > k)
+                return false;
+            return true;
+        }
+    }
+}
