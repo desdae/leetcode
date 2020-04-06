572f3ac4fb16d3a8cf461c2b54bc30a02bf0708f
Des Dae <desdae@gmail.com>
2020-04-06 11:56:37 +0300

    1395. Count Number of Teams

---
+++ CSharpSolutions/CSharpSolutions/01300/01395/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpSolutions._01300._01395
+{
+    public class Solution
+    {
+        public int NumTeams(int[] rating)
+        {
+            int n = rating.Length;
+            int r = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = i + 1; j < n; j++)
+                    for (int k = j + 1; k < n; k++)
+                        if ((rating[i] < rating[j] && rating[j] < rating[k]) || (rating[i] > rating[j] && rating[j] > rating[k]))
+                            r++;
+
+            return r;
+        }
+    }
+}
