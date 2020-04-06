3498963c62c92fb3e547ec364ffd652a5f684a0c
Des Dae <desdae@gmail.com>
2020-04-06 12:06:14 +0300

    1399. Count Largest Group

---
+++ CSharpSolutions/CSharpSolutions/01300/01399/Solution.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpSolutions._01300._01399
+{
+    public class Solution
+    {
+        private int SumDig(int n)
+        {
+            int r = 0;
+            while (n >= 1)
+            {
+                int d = n % 10;
+                n /= 10;
+                r += d;
+            }
+            return r;
+        }
+        public int CountLargestGroup(int n)
+        {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                int s = SumDig(i);
+                if (!d.ContainsKey(s))
+                    d[s] = 0;
+                d[s]++;
+            }
+            int max = d.Values.Max();
+            return d.Values.Count(x => x == max);
+
+        }
+    }
+}
