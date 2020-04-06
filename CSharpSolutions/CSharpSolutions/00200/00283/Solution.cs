adb1a89017e646d99d18d57622a06c1e9637e35b
Des Dae <desdae@gmail.com>
2020-04-06 12:03:50 +0300

    283. Move Zeroes

---
+++ CSharpSolutions/CSharpSolutions/00200/00283/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace CSharpSolutions._00200._00283
+{
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int n = nums.Length;
+            int p = 0;
+            int zc = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] == 0)
+                    zc++;
+                else
+                    nums[p++] = nums[i];
+            }
+            for (int i = n - 1; i >= n - zc; i--)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
