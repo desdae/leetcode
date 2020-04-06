265f20a1da21017086a18cfb6c7ef5c6dead0a87
Des Dae <desdae@gmail.com>
2020-04-06 12:11:32 +0300

    1402. Reducing Dishes

---
+++ CSharpSolutions/CSharpSolutions/01400/01402/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpSolutions._01400._01402
+{
+    public class Solution
+    {
+        public int MaxSatisfaction(int[] satisfaction)
+        {
+            Array.Sort(satisfaction);
+            int n = satisfaction.Length;
+            int max = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int sat = 0;
+                int k = 0;
+                for (int j = i; j < n; j++)
+                {
+                    k++;
+                    sat += k * satisfaction[j];
+                }
+                max = Math.Max(sat, max);
+            }
+            return max;
+        }
+    }
+}
