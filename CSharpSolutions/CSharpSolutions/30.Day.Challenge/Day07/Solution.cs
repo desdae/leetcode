26f3fe8007dc4faece8b377bb56aad0ad4e002f3
Des Dae <desdae@gmail.com>
2020-04-07 16:20:10 +0300

    30 day challange day 7. Counting Elements

---
+++ CSharpSolutions/CSharpSolutions/30.Day.Challenge/Day07/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpSolutions._30Day.Day07
+{
+    public class Solution
+    {
+        public int CountElements(int[] arr)
+        {
+            var d = new HashSet<int>();
+            arr.ToList().ForEach(x => d.Add(x));
+            return arr.Count(x => d.Contains(x + 1));            
+        }
+    }
+}
