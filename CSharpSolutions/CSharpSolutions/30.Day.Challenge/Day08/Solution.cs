4d7c919300763ad06f591d480bad1dc1424fa918
Des Dae <desdae@gmail.com>
2020-04-09 09:51:42 +0300

    30 day challange, day 8. Middle of the Linked List

---
+++ CSharpSolutions/CSharpSolutions/30.Day.Challenge/Day08/Solution.cs
@@ -0,0 +1,41 @@
+﻿using CSharpSolutions.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._30.Day.Challenge.Day08
+{
+    public class Solution
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            if (head == null)
+                return null;
+            if (head.next == null)
+                return head;
+            var h = head;
+            int c = 0;
+            while (h != null)
+            {
+                c++;
+                h = h.next;
+            }
+            int m;
+            if (c % 2 == 0)
+                m = c / 2;
+            else
+                m = (c - 1) / 2;
+            h = head;
+            c = 0;
+            while (true)
+            {
+                h = h.next;
+                c++;
+                if (c == m)
+                    return h;
+            }
+        }
+    }
+}
