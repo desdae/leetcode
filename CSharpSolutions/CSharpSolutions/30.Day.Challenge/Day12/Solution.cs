a9b39bfb66a46249c6e03a63ee7e0a89d2c99ef0
Des Dae <desdae@gmail.com>
2020-04-12 16:39:37 +0300

    30 day challenge, day 12. Last Stone Weight

---
+++ CSharpSolutions/CSharpSolutions/30.Day.Challenge/Day12/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._30.Day.Challenge.Day12
+{
+    public class Solution
+    {
+        public int LastStoneWeight(int[] stones)
+        {            
+            Array.Sort(stones);
+            int n = stones.Length;
+            while (n > 1 && stones[n - 2] > 0 )
+            {                
+                stones[n - 1] = Math.Abs(stones[n - 1] - stones[n - 2]); 
+                stones[n - 2] = 0;
+                Array.Sort(stones);
+            }
+            return stones[n - 1];
+        }
+    }
+}
