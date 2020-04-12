d98ab99d5b516801b5ee939bbee4ff720d51c7c6
Des Dae <desdae@gmail.com>
2020-04-12 08:37:48 +0300

    1410. HTML Entity Parser

---
+++ CSharpSolutions/CSharpSolutions/01400/01410/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpSolutions._01400._01410
+{
+    public class Solution
+    {
+        public string EntityParser(string text)
+        {
+            return System.Net.WebUtility.HtmlDecode(text).Replace("⁄", "/");
+        }
+    }
+}
