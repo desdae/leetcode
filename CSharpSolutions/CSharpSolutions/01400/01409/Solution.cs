48836b8bec65899a1fb5e8ccefb36a4dce4483e9
Des Dae <desdae@gmail.com>
2020-04-12 08:34:41 +0300

    1409. Queries on a Permutation With Key

---
+++ CSharpSolutions/CSharpSolutions/01400/01409/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpSolutions._01400._01409
+{
+    public class Solution
+    {
+        public int[] ProcessQueries(int[] queries, int m)
+        {
+            var p = new LinkedList<int>();
+            var r = new int[queries.Length];
+            for (int i = 1; i <= m; i++)
+                p.AddLast(i);
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int j = 0;
+                foreach (var x in p)
+                {
+                    if (queries[i] == x)
+                    {
+                        r[i] = j;
+                        p.Remove(x);
+                        p.AddFirst(x);
+                        break;
+                    }
+                    j++;
+                }
+            }
+            return r;
+        }
+    }
+}
