49ee7db96a0f1f278b9f908b752dd30fae1fa8c4
Des Dae <desdae@gmail.com>
2020-04-12 08:33:16 +0300

    1408. String Matching in an Array

---
+++ CSharpSolutions/CSharpSolutions/01400/01408/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpSolutions._01400._01408
+{
+    public class Solution
+    {
+        public IList<string> StringMatching(string[] words)
+        {
+            var ret = new List<string>();
+            for (int i = 0; i < words.Length; i++)
+                for (int j = 0; j < words.Length; j++)
+                    if (i != j && words[i].Contains(words[j]))
+                        if (!ret.Contains(words[j])) ret.Add(words[j]);
+            return ret;
+
+        }
+    }
+}
