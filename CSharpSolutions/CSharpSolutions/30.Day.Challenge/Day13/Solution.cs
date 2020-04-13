496185b69359a81dc14e88b47f707fe997f66038
Des Dae <desdae@gmail.com>
2020-04-13 21:18:06 +0300

    30 day challenge, day 13. Contiguous Array

---
+++ CSharpSolutions/CSharpSolutions/30.Day.Challenge/Day13/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._30.Day.Challenge.Day13
+{
+    public class Solution
+    {
+        public int FindMaxLength(int[] nums)
+        {
+            Dictionary<int, int> d = new Dictionary<int, int>();
+            d[0] = -1;
+            int maxl = 0;
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                count += nums[i] == 1 ? 1 : -1;
+                if (d.ContainsKey(count))
+                {
+                    maxl = Math.Max(maxl, i - d[count]);
+                }
+                else
+                {
+                    d[count] = i;
+                }
+            }
+            return maxl;
+        }
+    }
+}
