c93ba3cd2f2c9dc6ad49a4a513c2ccd1d1826dba
Des Dae <desdae@gmail.com>
2020-04-14 17:47:51 +0300

    784. Letter Case Permutation

---
+++ CSharpSolutions/CSharpSolutions/00700/00784/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._00700._00784
+{
+    public class Solution
+    {
+        private List<string> results = new List<string>();
+
+        private void Perm(int pos, StringBuilder sb)
+        {
+            while (pos < sb.Length && Char.IsDigit(sb[pos]))
+                pos++;
+            if (pos < sb.Length)
+            {
+                sb[pos] = sb[pos].ToString().ToUpper()[0];
+                Perm(pos + 1, new StringBuilder(sb.ToString()));
+                sb[pos] = sb[pos].ToString().ToLower()[0];
+                Perm(pos + 1, new StringBuilder(sb.ToString()));
+            }
+            else
+            {
+                results.Add(sb.ToString());
+                return;
+            }
+        }
+        public IList<string> LetterCasePermutation(string S)
+        {
+
+            Perm(0, new StringBuilder(S));
+            return results;
+        }
+    }
+}
