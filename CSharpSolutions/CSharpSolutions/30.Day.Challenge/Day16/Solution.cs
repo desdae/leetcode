c674c30077281a4c0d2c7f4fa2d49d2682064564
Des Dae <desdae@gmail.com>
2020-04-16 16:56:28 +0300

    30 day challenge, day 16. Valid Parenthesis String

---
+++ CSharpSolutions/CSharpSolutions/30.Day.Challenge/Day16/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._30.Day.Challenge.Day16
+{
+    public class Solution
+    {
+        private bool flag;
+        private void DFS(int x, int o, int c, StringBuilder sb)
+        {
+            if (c > o)
+                return; 
+            if (flag)
+                return;
+            if (x == sb.Length)
+            {
+                if (c == o)
+                    flag = true;
+                return;
+            }
+            if (sb[x] == '*')
+            {
+                DFS(x + 1, o, c, sb);
+                DFS(x + 1, o + 1, c, sb);
+                DFS(x + 1, o, c + 1, sb);
+            } 
+            else if (sb[x] == '(')
+            {
+                DFS(x + 1, o + 1, c, sb);
+            }
+            else
+            {
+                DFS(x + 1, o, c + 1, sb);
+            }
+        }
+        public bool CheckValidString(string s)
+        {
+            DFS(0, 0, 0, new StringBuilder(s));
+            return flag;
+        }
+    }
+}
