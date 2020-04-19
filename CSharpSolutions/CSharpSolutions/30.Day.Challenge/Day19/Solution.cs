c5e3a29c08ccdc07a57f6771f1fa2572aa768fba
Des Dae <desdae@gmail.com>
2020-04-19 13:37:02 +0300

    30 days of code, day 19. Search in Rotated Sorted Array

---
+++ CSharpSolutions/CSharpSolutions/30.Day.Challenge/Day19/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._30.Day.Challenge.Day19
+{
+    public class Solution
+    {
+        public int Search(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                    return i;
+            }
+            return -1;
+        }
+    }
+}
