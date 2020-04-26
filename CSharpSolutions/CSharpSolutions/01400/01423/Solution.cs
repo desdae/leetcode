aac5b644fc292e83d5785cc894c2cdf812eeafba
Des Dae <desdae@gmail.com>
2020-04-26 10:13:36 +0300

    1423. Maximum Points You Can Obtain from Cards

---
+++ CSharpSolutions/CSharpSolutions/01400/01423/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._01400._01423
+{
+    public class Solution
+    {
+        public int MaxScore(int[] cardPoints, int k)
+        {
+            int s1 = 0;
+            int s2 = cardPoints.Skip(Math.Max(0, cardPoints.Count() - k)).Sum();
+            int max = s1 + s2;
+            for (int i = 0; i < k; i++)
+            {
+                s1 += cardPoints[i];
+                s2 -= cardPoints[cardPoints.Length - k + i];
+                max = Math.Max(max, s1 + s2);
+            }
+            return max;
+        }
+    }
+}
