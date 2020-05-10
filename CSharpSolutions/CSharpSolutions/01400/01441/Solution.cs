e7191569508bd43431e0e62fef4c6604283ced0a
Des Dae <desdae@gmail.com>
2020-05-10 13:01:57 +0300

    1441. Build an Array With Stack Operations

---
+++ CSharpSolutions/CSharpSolutions/01400/01441/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._01400._01441
+{
+    public class Solution
+    {
+        public IList<string> BuildArray(int[] target, int n)
+        {
+            var r = new List<string>();
+            for (int i = 1; i <= n && i <= target[target.Length - 1]; i++)
+            {
+                r.Add("Push");
+                if (!target.Contains(i))
+                {
+                    r.Add("Pop");
+                }
+            }
+            return r;
+        }
+    }
+}
