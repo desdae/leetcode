12661f94d7ec7c41da0010e10155fdf4d0758e0d
Des Dae <desdae@gmail.com>
2020-05-10 13:03:39 +0300

    1442. Count Triplets That Can Form Two Arrays of Equal XOR

---
+++ CSharpSolutions/CSharpSolutions/01400/1442/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharpSolutions._01400._1442
+{
+    public class Solution
+    {
+        public int CountTriplets(int[] arr)
+        {
+            int n = arr.Length;
+            int[,] xor = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                int x = arr[i];
+                xor[i, i] = x;
+                for (int j = i + 1; j < n; j++)
+                {
+                    x = x ^ arr[j];
+                    xor[i, j] = x;
+                }
+            }
+            int r = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = i + 1; j < n; j++)
+                    for (int k = j; k < n; k++)
+                        if (xor[i, j - 1] == xor[j, k])
+                            r++;
+
+            return r;
+        }
+    }
+}
