8acadf3e2ae50d3828014fc9ad4fb81315d3b60a
Des Dae <desdae@gmail.com>
2020-05-16 19:55:14 +0300

    1446. Consecutive Characters

---
+++ CSharpSolutions/CSharpSolutions/01400/01446/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpSolutions._01400._01446
+{
+    public class Solution
+    {
+        public int MaxPower(string s)
+        {
+            char c = s[0];
+            int l = 0;
+            int m = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != c)
+                {
+                    m = Math.Max(m, l);
+                    l = 1;
+                    c = s[i];
+                }
+                else
+                {
+                    l++;
+                }
+            }
+            return Math.Max(m, l);
+        }
+    }
+}
