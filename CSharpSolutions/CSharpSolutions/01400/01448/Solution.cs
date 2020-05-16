2ce882342e64a7fd9c9f54d88b695e999e489946
Des Dae <desdae@gmail.com>
2020-05-16 19:59:43 +0300

    1448. Count Good Nodes in Binary Tree

---
+++ CSharpSolutions/CSharpSolutions/01400/01448/Solution.cs
@@ -0,0 +1,26 @@
+﻿using CSharpSolutions.Common;
+using System;
+
+namespace CSharpSolutions._01400._01448
+{
+    public class Solution
+    {
+        int good;
+        private void DFS(int max, TreeNode root)
+        {
+            if (root == null)
+                return;
+            if (root.val >= max)
+                good++;
+            max = Math.Max(root.val, max);
+            DFS(max, root.left);
+            DFS(max, root.right);
+        }
+        public int GoodNodes(TreeNode root)
+        {
+            good = 0;
+            DFS(int.MinValue, root);
+            return good;
+        }
+    }
+}
