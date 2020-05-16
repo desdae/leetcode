1b0f1f21756124058bf680ff2119f592d8cf70ca
Des Dae <desdae@gmail.com>
2020-05-16 21:29:14 +0300

    1449. Form Largest Integer With Digits That Add up to Target

---
+++ CSharpSolutions/CSharpSolutions/01400/01449/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace CSharpSolutions._01400._01449
+{
+    public class Solution
+    {
+        public string LargestNumber(int[] cost, int target)
+        {
+            BigInteger[] dp = new BigInteger[target + 1];
+            for (int i = 0; i < target + 1; i++)
+                dp[i] = -1;
+            dp[0] = 0;
+            for (int i = 0; i < target + 1; i++)
+                for (int j = 0; j < 9; j++)
+                {
+                    if (i - cost[j] >= 0 && dp[i - cost[j]] >= 0)
+                    {
+                        var i1 = dp[i - cost[j]] * 10 + j + 1;
+                        var i2 = dp[i];
+                        dp[i] = i1 > i2 ? i1 : i2;
+                    }
+                }
+
+            if (dp[target] < 0)
+                return "0";
+            return dp[target].ToString();
+        }
+    }
+}
