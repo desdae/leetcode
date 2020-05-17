8b512b60ae73ee4def09318307491a9952fb9d56
Des Dae <desdae@gmail.com>
2020-05-17 08:18:58 +0300

    1450. Number of Students Doing Homework at a Given Time [Solution.cs]

---
+++ CSharpSolutions/CSharpSolutions/01400/01450/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._01400._01450
+{
+    public class Solution
+    {
+        public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+        {
+            int res = 0;
+            for (int i = 0; i < startTime.Length; i++)
+            {
+                if (startTime[i] <= queryTime && endTime[i] >= queryTime)
+                    res++;
+            }
+
+            return res;
+        }
+    }
+}
