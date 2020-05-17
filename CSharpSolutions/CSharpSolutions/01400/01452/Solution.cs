c5520ac2fd1b64d6351547bd2e3e9122c1593efe
Des Dae <desdae@gmail.com>
2020-05-17 08:20:53 +0300

    1452. People Whose List of Favorite Companies Is Not a Subset of Another List

---
+++ CSharpSolutions/CSharpSolutions/01400/01452/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpSolutions._01400._01452
+{
+    public class Solution
+    {
+        public IList<int> PeopleIndexes(IList<IList<string>> favoriteCompanies)
+        {
+            var res = new List<int>();
+
+            var x = favoriteCompanies;
+
+            var h = new List<HashSet<string>>();
+
+            foreach (var c in x)
+                h.Add(c.ToHashSet());
+
+            for (int i = 0; i < h.Count; i++)
+            {
+                bool b = false;
+                for (int j = 0; j < h.Count; j++)
+                {
+                    if (i != j && h[i].IsSubsetOf(h[j]))
+                    {
+                        b = true;
+                        break;
+                    }
+                }
+                if (!b)
+                    res.Add(i);
+            }
+
+            return res;
+        }
+    }
+}
