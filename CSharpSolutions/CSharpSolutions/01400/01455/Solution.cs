f3d46789c9f65439ddcf264e13a342f7ecd5ded7
Des Dae <desdae@gmail.com>
2020-05-24 08:47:49 +0300

    1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ CSharpSolutions/CSharpSolutions/01400/01455/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._01400._01455
+{
+    public class Solution
+    {
+        public int IsPrefixOfWord(string sentence, string searchWord)
+        {
+            var w = sentence.Split(' ');
+            for (int i = 0; i < w.Length; i++)
+            {
+                if (w[i].StartsWith(searchWord))
+                    return i + 1;
+            }
+            return -1;
+        }
+    }
+}
