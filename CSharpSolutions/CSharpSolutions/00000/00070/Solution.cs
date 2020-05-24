af9083fa9c5448dee29c8173d373367b361ce98c
Des Dae <desdae@gmail.com>
2020-05-24 15:38:14 +0300

    70. Climbing Stairs

---
+++ CSharpSolutions/CSharpSolutions/00000/00070/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSolutions._00000._00070
+{
+    public class Solution
+    {
+        private Dictionary<int, int> dp = new Dictionary<int, int>();
+        
+        private int Calc(int n)
+        {
+            if (dp.ContainsKey(n))
+                return dp[n];
+            int res = 0;
+            if (n == 1)
+                res = 1;
+            else if (n == 2)
+                res = 2;
+            else
+                res = Calc(n - 2) + Calc(n - 1);
+            dp[n] = res;
+            return res;
+        }
+        public int ClimbStairs(int n)
+        {
+            dp.Clear();
+            return Calc(n);
+        }
+    }
+}
