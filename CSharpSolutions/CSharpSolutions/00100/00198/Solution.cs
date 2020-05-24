589e674a677fa3a85fe1548837d9698ee348a252
Des Dae <desdae@gmail.com>
2020-05-24 16:14:56 +0300

    198. House Robber

---
+++ CSharpSolutions/CSharpSolutions/00100/00198/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace CSharpSolutions._00100._00198
+{
+    public class Solution
+    {
+        public int Rob(int[] nums)
+        {
+            int n = nums.Length;
+            if (n == 0)
+                return 0;
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+            dp[1] = nums[0];
+            for (int i = 1; i < n; i++)
+                dp[i + 1] = Math.Max(dp[i], dp[i - 1] + nums[i]);
+            return dp[n];
+        }
+    }
+}
