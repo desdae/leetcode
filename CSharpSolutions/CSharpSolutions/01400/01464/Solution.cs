1bebfe0cee1f0828e184d95555f1bc16af489999
Des Dae <desdae@gmail.com>
2020-05-31 11:25:50 +0300

    1464. Maximum Product of Two Elements in an Array

---
+++ CSharpSolutions/CSharpSolutions/01400/01464/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace CSharpSolutions._01400._01464
+{
+    public class Solution
+    {
+        public int MaxProduct(int[] nums)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = 0; j < nums.Length; j++)
+                    if (i != j)
+                        max = Math.Max((nums[i] - 1) * (nums[j] - 1), max);
+            return max;
+        }
+    }
+}
