8f08187a51bfa1e5377ce6ecd77ba12c2c20cb03
Des Dae <desdae@gmail.com>
2020-06-07 13:47:41 +0300

    1470. Shuffle the Array

---
+++ CSharpSolutions/CSharpSolutions/01400/01470/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpSolutions._01400._01470
+{
+    public class Solution
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            List<int> res = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                res.Add(nums[i]);
+                res.Add(nums[n + i]);
+            }
+            return res.ToArray();
+        }
+    }
+}
