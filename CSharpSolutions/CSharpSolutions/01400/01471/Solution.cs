8db9859088cf1d7e3aca7a40ba09aded1cbeb8e6
Des Dae <desdae@gmail.com>
2020-06-07 13:49:29 +0300

    1471. The k Strongest Values in an Array

---
+++ CSharpSolutions/CSharpSolutions/01400/01471/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpSolutions._01400._01471
+{
+    public class Solution
+    {
+        public int[] GetStrongest(int[] arr, int k)
+        {
+            int n = arr.Length;
+            int mi = (n - 1) / 2;
+            var l = arr.ToList();
+            l.Sort();
+            int m = l[mi];
+            Array.Sort(arr, (a, b) =>
+            {
+                if (Math.Abs(a - m) == Math.Abs(b - m))
+                    return b - a;
+                return Math.Abs(b - m) - Math.Abs(a - m);
+            });
+            return arr.Take(k).ToArray();
+        }
+    }
+}
