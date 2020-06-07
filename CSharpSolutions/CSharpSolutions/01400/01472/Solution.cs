77db1ccda53664ab9f14320480db643696c86b43
Des Dae <desdae@gmail.com>
2020-06-07 13:52:06 +0300

    1472. Design Browser History

---
+++ CSharpSolutions/CSharpSolutions/01400/01472/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpSolutions._01400._01472
+{
+    public class BrowserHistory
+    {
+        List<string> h = new List<string>();
+        int idx = 0;
+
+        public BrowserHistory(string homepage)
+        {
+            Visit(homepage);
+        }
+
+        public void Visit(string url)
+        {
+            if (idx < h.Count - 1)
+                h.RemoveRange(idx + 1, h.Count - idx - 1);
+            h.Add(url);
+            idx = h.Count - 1;
+        }
+
+        public string Back(int steps)
+        {
+            idx = Math.Max(idx - steps, 0);
+            return h[idx];
+        }
+
+        public string Forward(int steps)
+        {
+            idx = Math.Min(idx + steps, h.Count - 1);
+            return h[idx];
+        }
+    }
+}
