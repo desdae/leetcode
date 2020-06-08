42265bfa9d1563fdfc53c0e211952219cc17dc0d
Des Dae <desdae@gmail.com>
2020-06-08 13:26:40 +0300

    1460. Make Two Arrays Equal by Reversing Sub-arrays

---
+++ CSharpSolutions/CSharpSolutions/01400/01460/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpSolutions._01400._01460
+{
+    public class Solution
+    {
+        public bool CanBeEqual(int[] target, int[] arr)
+        {
+            Array.Sort(target);
+            Array.Sort(arr);
+            return Enumerable.SequenceEqual(target, arr);
+        }
+    }
+}
