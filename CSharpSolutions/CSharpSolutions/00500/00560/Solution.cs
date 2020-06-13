5128c6e063ca2a748df577cc423e0fad3dfbdf92
Des Dae <desdae@gmail.com>
2020-06-13 22:00:56 +0300

    560. Subarray Sum Equals K

---
+++ CSharpSolutions/CSharpSolutions/00500/00560/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpSolutions._00500._00560
+{
+    public class Solution
+    {
+        private void Add(Dictionary<int, List<int>> d, int key, int val)
+        {
+            if (!d.ContainsKey(key))
+                d[key] = new List<int>();
+            d[key].Add(val);
+        }
+        public int SubarraySum(int[] nums, int k)
+        {
+            var sums = new Dictionary<int, List<int>>();
+            int sum = 0;
+            int res = 0;
+            Add(sums, 0, -1);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i]; 
+                if (sums.ContainsKey(sum - k))
+                    res+=sums[sum - k].Count;
+                Add(sums, sum, i);
+            }
+            return res;
+        }
+    }
+}
