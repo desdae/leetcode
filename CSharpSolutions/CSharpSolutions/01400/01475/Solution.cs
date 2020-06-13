896263db545f4710b477bc991a0906713d227aaf
Des Dae <desdae@gmail.com>
2020-06-13 20:47:58 +0300

    1475. Final Prices With a Special Discount in a Shop

---
+++ CSharpSolutions/CSharpSolutions/01400/01475/Solution.cs
@@ -0,0 +1,25 @@
+﻿
+namespace CSharpSolutions._01400._01475
+{
+    public class Solution
+    {
+        public int[] FinalPrices(int[] prices)
+        {
+            for (int i = 0; i < prices.Length; i++)
+            {
+                var p = prices[i];
+                int m = 0;
+                for (int j = i + 1; j < prices.Length; j++)
+                {
+                    if (p >= prices[j])
+                    {
+                        m = prices[j];
+                        break;
+                    }
+                }
+                prices[i] = p - m;
+            }
+            return prices;
+        }
+    }
+}
