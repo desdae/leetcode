3dc30cd72fc1baac464b4825f41d1aa29aa16bb3
Des Dae <desdae@gmail.com>
2020-06-16 16:02:00 +0300

    1480. Running Sum of 1d Array

---
+++ CSharpSolutions/CSharpSolutions/01400/01480/Solution.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace CSharpSolutions._01400._01480
+{
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                nums[i] = sum;
+            }
+            return nums;
+        }
+    }
+}
