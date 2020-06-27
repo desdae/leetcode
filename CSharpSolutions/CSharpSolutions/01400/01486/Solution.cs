81dcddc331a998468243e630af08a7fab8fad3c9
Des Dae <desdae@gmail.com>
2020-06-27 17:28:12 +0300

    1486. XOR Operation in an Array

---
+++ CSharpSolutions/CSharpSolutions/01400/01486/Solution.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CSharpSolutions._01400._01486
+{
+    class Solution
+    {
+        public int XorOperation(int n, int start)
+        {
+            var x = start;
+            for (int i = 1; i < n; i++)
+            {
+                x = x ^ (start + i * 2);
+            }
+            return x;
+        }
+    }
+}
