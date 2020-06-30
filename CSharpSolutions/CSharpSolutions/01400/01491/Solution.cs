d6b95ddb5f6c7daca30a5913dde404c2e3f4fb42
Des Dae <desdae@gmail.com>
2020-06-30 06:49:20 +0300

    1491. Average Salary Excluding the Minimum and Maximum Salary

---
+++ CSharpSolutions/CSharpSolutions/01400/01491/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace CSharpSolutions._01400._01491
+{
+    class Solution
+    {
+        public double Average(int[] salary)
+        {
+            int max = salary.Max();
+            int min = salary.Min();
+            int sum = 0;
+            int count = 0;
+            for (int i = 0; i < salary.Length; i++)
+            {
+                if (salary[i] != max && salary[i] != min)
+                {
+                    sum += salary[i];
+                    count++;
+                }
+            }
+            return (double)sum / count;
+        }
+    }
+}
