8c7c35d374309c20a7e7192ac5cd99edc5989622
Des Dae <desdae@gmail.com>
2020-06-30 06:51:31 +0300

    1492. The kth Factor of n

---
+++ CSharpSolutions/CSharpSolutions/01400/01492/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpSolutions._01400._01492
+{
+    class Solution
+    {
+        public int KthFactor(int n, int k)
+        {
+            var f = new List<int>();
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    f.Add(i);
+            }
+            if (k > f.Count)
+                return -1;
+            return f[k - 1];
+        }
+    }
+}
