f1861a44e94a037cfaa0b4a462540e3fa59400db
Des Dae <desdae@gmail.com>
2020-07-12 17:35:16 +0300

    1502. Can Make Arithmetic Progression From Sequence

---
+++ CSharpSolutions/CSharpSolutions/01500/01502/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharpSolutions._01500._01502
+{
+    public class Solution
+    {
+        public bool CanMakeArithmeticProgression(int[] arr)
+        {
+            if (arr.Length == 2)
+                return true;
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                if (arr[i + 1] - arr[i] != arr[i + 2] - arr[i + 1])
+                    return false;
+            }
+            return true;
+        }
+    }
+}
