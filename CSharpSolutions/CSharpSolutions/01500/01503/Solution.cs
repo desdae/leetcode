01cc725de2b1d10cbedfae5c878dbf38e28b21e0
Des Dae <desdae@gmail.com>
2020-07-12 17:37:07 +0300

    Last Moment Before All Ants Fall Out of a Plank

---
+++ CSharpSolutions/CSharpSolutions/01500/01503/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpSolutions._01500._01503
+{
+    public class Solution
+    {
+        public int GetLastMoment(int n, int[] left, int[] right)
+        {
+            int max = 0;
+            for (int i = 0; i < left.Length; i++)
+                max = Math.Max(max, left[i]);
+            for (int i = 0; i < right.Length; i++)
+                max = Math.Max(max, n - right[i]);
+
+            return max;
+        }
+    }
+}
